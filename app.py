0861e3e1db65d55955d006a7fa95958cd3984146
BlackFishY <86031075+BlackFishY@users.noreply.github.com>
2022-03-21 12:17:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1477,8 +1477,7 @@ def handle_message(event):
                                                      " 🛑此為群組機器人🛑\n"
                                                      "🛑私聊版本還未開放🛑"))
     elif msg == "!個人資料":
-        user = user_id
-        bot.reply_message(chatToken, TextSendMessage(f"您的user_ID為:{user_id}" + "Type" + type(user)))
+        bot.reply_message(chatToken, TextSendMessage(f"您的user_ID為:{user_id}"))
     elif msg == "!群組資料":
         team = bot.get_group_summary(event.source.group_id)
         group_count = bot.get_group_members_count(event.source.group_id)
