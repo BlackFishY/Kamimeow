61d8224927b4f3fb728ec0c6dc02927adebcf278
BlackFishY <86031075+BlackFishY@users.noreply.github.com>
2022-04-05 15:46:55 +0800

    Add files via upload

--- commands.py
+++ commands.py
@@ -60,8 +60,8 @@ class Commands:
                 take = bot.get_group_member_profile(group_id, user)
                 mentiontake = bot.get_group_member_profile(group_id, mentionuser)
                 deposit = R_deposit.find_one({"user": mentionuser})
-                R_deposit.update_one({"user": mentionuser}, {"$set": {"deposit": round(deposit['deposit'] + 7, 2)}})
-                self.bot.reply_message(self.token, TextSendMessage("成功新增該使用者7元"))
+                R_deposit.update_one({"user": mentionuser}, {"$set": {"deposit": round(deposit['deposit'] + 30, 2)}})
+                self.bot.reply_message(self.token, TextSendMessage("成功新增該使用者30元"))
             except AttributeError:
                 self.bot.reply_message(self.token, TextSendMessage("請@正確目標對象"))
                 return
